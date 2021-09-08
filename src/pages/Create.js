f6e505d23579ea67a20d08bcea1276dca9b6e71b
ukaran11 <ukaran11@gmail.com>
2021-09-09 01:31:13 +0530

    Utsav | Added form, button & custom theme

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Typography, Button, Container, TextField } from "@material-ui/core";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "block",
+  },
+});
 
 export default function Create() {
+  const classes = useStyles();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+    
+    if (title === "") {
+      setTitleError(true);
+    }
+
+    if (details === "") {
+      setDetailsError(true);
+    }
+
+    if (title && details) {
+      console.log(title, details);
+    }
+  };
+
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
   return (
-    <div>
-      Create page
-    </div>
-  )
+    <Container>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.field}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          className={classes.field}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
+
+        <Button
+          className={classes.btn}
+          type="submit"
+          color="secondary"
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
+  );
 }
