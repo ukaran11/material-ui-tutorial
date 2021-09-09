38a16e1fdc49a07aaba48317eb8346d075f2577a
ukaran11 <ukaran11@gmail.com>
2021-09-09 18:31:27 +0530

    Utsav | Using json-server setup-ed fetching and posting of notes

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -11,6 +11,7 @@ import {
 } from "@material-ui/core";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 import { makeStyles, FormControlLabel } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles({
   field: {
@@ -22,6 +23,7 @@ const useStyles = makeStyles({
 
 export default function Create() {
   const classes = useStyles();
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -38,6 +40,11 @@ export default function Create() {
 
     if (title && details) {
       console.log(title, details, category);
+      fetch('http://localhost:8000/notes', {
+        method: 'POST',
+        headers: {"Content-type":"application/json"},
+        body: JSON.stringify({title, details, category})
+      }).then(() => history.push('/'))
     }
   };
 
