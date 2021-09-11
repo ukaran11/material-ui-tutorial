cd81a3354648fe563adbae0826722bf08b69b83a
ukaran11 <ukaran11@gmail.com>
2021-09-12 00:23:00 +0530

    Utsav | Added basic Drawer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,43 @@
 import react from 'react';
 import { makeStyles } from '@material-ui/core';
+import Drawer from '@material-ui/core/Drawer';
+import { Typography } from '@material-ui/core';
+
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles({
     page: {
         background: '#f9f9f9',
         width: '100%'
+    },
+    drawer: {
+        width: drawerWidth
+    },
+    drawerPaper: {
+        width: drawerWidth
+    },
+    root: {
+        display: 'flex',
     }
 })
 export default function Layout({ children }) {
     const classes = useStyles();
-    
+
     return (
-        <div>
+        <div className={classes.root}>
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                anchor="left"
+                classes={{ paper: classes.drawerPaper}}
+            >
+                <div>
+                    <Typography variant="h5">
+                        Ninja Notes
+                    </Typography>
+                </div>
+            </Drawer>
             <div className={classes.page}>
                 { children }
             </div>
