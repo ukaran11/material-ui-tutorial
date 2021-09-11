e99f1b0ab2e9a937605cf36e68193f325f48718d
ukaran11 <ukaran11@gmail.com>
2021-09-11 17:42:27 +0530

    Utsav | Added the Layout wrapper comp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Notes from "./pages/Notes";
-import Create from "./pages/Create";
 import { createTheme, ThemeProvider } from "@material-ui/core";
 import { purple } from "@material-ui/core/colors";
+
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
+import Layout from "./components/Layout";
 
 const theme = createTheme({
   palette: {
@@ -23,14 +25,16 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <Notes />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-        </Switch>
+        <Layout>
+          <Switch>
+            <Route exact path="/">
+              <Notes />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </Layout>
       </Router>
     </ThemeProvider>
   );
---
+++ src/components/Layout.js
@@ -0,0 +1,20 @@
+import react from 'react';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    page: {
+        background: '#f9f9f9',
+        width: '100%'
+    }
+})
+export default function Layout({ children }) {
+    const classes = useStyles();
+    
+    return (
+        <div>
+            <div className={classes.page}>
+                { children }
+            </div>
+        </div>
+    );
+};
