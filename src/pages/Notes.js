bc696bb9b24ac4d9478d15a9a8e2ce4e6f10aa3d
ukaran11 <ukaran11@gmail.com>
2021-09-15 01:51:25 +0530

    Utsav | Added React masonry grid

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -3,7 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { Container } from '@material-ui/core';
 import NoteCard from '../components/NoteCard';
-
+import Masonry from 'react-masonry-css';
 export default function Notes() {
 
   const [notes, setNotes] = useState([]);
@@ -22,6 +22,13 @@ export default function Notes() {
     const newNotes = notes.filter(note=> note.id !== id);
     setNotes(newNotes);
   }
+
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1
+  }
+
   return (
     <Container>
       {/* <Grid container>
@@ -39,13 +46,21 @@ export default function Notes() {
         </Grid>
       </Grid> */}
 
-      <Grid container spacing={3}>
+      {/* <Grid container spacing={3}>
+      </Grid> */}
+
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"      
+      >
         {notes.map((note) => (
-          <Grid item key={note.id} xs={12} sm={6} md={3} lg={4}>
+          <div item key={note.id}>
             <NoteCard note={note} handleDelete={handleDelete}/>
-          </Grid>
+          </div>
         ))}
-      </Grid>
+        </Masonry>
+      
     </Container>
   )
 }
